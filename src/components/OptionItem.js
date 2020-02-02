d5b2845c04239ae04b98b3640e76298471a3b898
wavinginspace <paulbaisley@gmail.com>
2020-02-02 14:09:24 -0500

    lint

--- src/components/OptionItem.js
+++ src/components/OptionItem.js
@@ -3,21 +3,21 @@ import slugify from 'slugify';
 
 function OptionItem(props) {
   const itemHash = slugify(JSON.stringify(props.item));
-      return (
-        <div key={itemHash} className="feature__item">
-          <input
-            type="radio"
-            id={itemHash}
-            className="feature__option"
-            name={slugify(props.feature)}
-            checked={props.item.name === props.ourState[props.feature].name}
-            onChange={e => props.updateFeature(props.feature, props.item)}
-          />
-          <label htmlFor={itemHash} className="feature__label">
-            {props.item.name} ({props.usCurrency.format(props.item.cost)})
-          </label>
-        </div>
-  )
+  return (
+    <div key={itemHash} className="feature__item">
+      <input
+        type="radio"
+        id={itemHash}
+        className="feature__option"
+        name={slugify(props.feature)}
+        checked={props.item.name === props.ourState[props.feature].name}
+        onChange={e => props.updateFeature(props.feature, props.item)}
+      />
+      <label htmlFor={itemHash} className="feature__label">
+        {props.item.name} ({props.usCurrency.format(props.item.cost)})
+      </label>
+    </div>
+  );
 }
 
-export default OptionItem
+export default OptionItem;
